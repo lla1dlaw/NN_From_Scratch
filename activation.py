77dc25b4c5428e31db3894f644bc59fcb7adbe4d
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-02-22 17:42:47 -0500

    Created activation functions in activation class

--- activation.py
+++ activation.py
@@ -2,7 +2,24 @@
 Filename: activation.py
 Author: Liam Laidlaw
 Date: February 22, 2025
-Purpose: 
+Purpose: Collection of activation functions for neural networks
 """
 
+import numpy as np
+
+class activation:
+    def __init__():
+        ...
+
+    def relu(x: float) -> float:
+        return np.maximum(0, x)
+
+    def sigmoid(x: float) -> float:
+        return 1/(1 + np.exp(-x))
+
+    def tanh(x: float) -> float:
+        return np.tanh(x)
+
+    def softplus(x: float) -> float:
+        return np.log(1 + np.exp(x))
 
